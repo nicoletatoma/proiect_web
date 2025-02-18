c1b29e9687378d90eab354c1423a3ce8303299bf
Nichita <Home@DESKTOP-6H408TA>
2025-02-18 14:54:35 +0200

    update

--- 10717proiect/App_Start/BundleConfig.cs
+++ 10717proiect/App_Start/BundleConfig.cs
@@ -13,7 +13,23 @@ namespace _10717proiect.App_Start
             void Application_Start(object sender, EventArgs e)
             {
 
-               
+                // Bootstrap style
+                bundles.Add(new
+                StyleBundle("~/bundles/bootstrap/css").Include("~/Content/bootstrap.min.css", new CssRewriteUrlTransform()));
+                // Bootstrap
+                bundles.Add(new ScriptBundle("~/bundles/bootstrap/js").Include(
+                "~/Scripts/bootstrap.min.js"));
+                //Bootstrap style
+                bundles.Add(new StyleBundle("~/bundles/bootstrap/css").Include( "~/Content/bootstrap.min.css", new CssRewriteUrlTransform()));
+                // Font Awesome icons style
+                bundles.Add(new StyleBundle("~/bundles/font-awesome/css").Include(
+                //Toaster
+                "~/Content/font-awesome.min.css", new CssRewriteUrlTransform()));
+                bundles.Add(new StyleBundle("~/bundles/toaster/css").Include(
+                "~/Vendors/toastr/toastr.min.css", new CssRewriteUrlTransform()));
+                //DataTables
+                bundles.Add(new StyleBundle("~/bundles/datatables/css").Include(
+                "~/Vendors/datatables/datatables.min.css", new CssRewriteUrlTransform()));
                 BundleConfig.RegisterBundles(BundleTable.Bundles);
             }
         }
