96ac3e9660550db17bd8eff3d9333a6a08ae076f
Nicoleta <nicoleta.toma@isa.utm.md>
2025-05-21 21:07:49 +0300

    Modificari in structura la Evenimente Controller

--- 10717proiect/Controllers/EvenimenteController.cs
+++ 10717proiect/Controllers/EvenimenteController.cs
@@ -4,23 +4,26 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using _10717proiect.BusinessLogic.DBContext;
+using _10717proiect.BusinessLogic.Interfaces;
 using _10717proiect.Domain.Model.Event;
 
 namespace _10717proiect.Controllers
 {
+
     public class EvenimenteController : Controller
     {
-        private readonly EventContext _context;
+        private readonly IEvent _product;
 
         public EvenimenteController()
         {
-            _context = new EventContext();
+            var bl = new BusinessLogic.BusinessLogic();
+            _product = bl.CreateEventBL();
         }
 
         public ActionResult Index()
         {
-            var evenimente = _context.Events.ToList(); 
-            return View(evenimente); 
+            var evenimente = _product.GetAllEvents();
+            return View(evenimente);
         }
     }
 }
