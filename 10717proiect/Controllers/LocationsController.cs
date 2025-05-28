e37231b6971d32433e6a343faf61b7fb75ca2d54
Nichita <Home@DESKTOP-6H408TA>
2025-05-28 13:26:20 +0300

    rezolvare erori la logica de adaugare locatii

--- 10717proiect/Controllers/LocationsController.cs
+++ 10717proiect/Controllers/LocationsController.cs
@@ -1,5 +1,6 @@
 ﻿using _10717proiect.BusinessLogic.Interfaces;
 using _10717proiect.Models.Event;
+using _10717proiect.Models.Location;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,12 +25,12 @@ namespace _10717proiect.Controllers
         }
           [HttpPost]
           [ValidateAntiForgeryToken]
-          public ActionResult CreateEvent(EventDataModelView createEvent)
+          public ActionResult CreateLocation(LocationDataView createLocation)
           {
                string imagePath = null;
 
 
-               if (createEvent.ImageFile != null && createEvent.ImageFile.ContentLength > 0)
+               if (createLocation.ImageFile != null && createLocation.ImageFile.ContentLength > 0)
                {
 
                     string uploadsFolder = Server.MapPath("~/Content/Images/Location");
@@ -42,33 +43,29 @@ namespace _10717proiect.Controllers
 
                     // Generați un nume unic pentru fișier pentru a evita suprascrierile
                     string uniqueFileName = Guid.NewGuid().ToString() + "_" +
-                                           System.IO.Path.GetFileName(createEvent.ImageFile.FileName);
+                                           System.IO.Path.GetFileName(createLocation.ImageFile.FileName);
                     string filePath = System.IO.Path.Combine(uploadsFolder, uniqueFileName);
 
                     // Salvați fișierul pe server
-                    createEvent.ImageFile.SaveAs(filePath);
+                    createLocation.ImageFile.SaveAs(filePath);
 
                     // Salvați calea relativă în baza de date
                     imagePath = "/Content/Images/Location/" + uniqueFileName;
                }
 
-               var data = new _10717proiect.Domain.Model.Event.LocationData
+               var data = new _10717proiect.Domain.Model.Location.LocationData
                {
-                    Name = createEvent.Name,
-                    Description = createEvent.Description,
-                    EventDate = createEvent.EventDate,
-                    Location = createEvent.Location,
-                    CategoryId = createEvent.CategoryId,
-                    Price = createEvent.Price,
-                    ImagePath = imagePath, // Folosiți calea nou generată
-                    CreatedAt = DateTime.Now
+                    Name = createLocation.Name,
+                    Description = createLocation.Description,
+                    Address = createLocation.Address,
+                    Phone = createLocation.Phone,
+                    ImagePath = imagePath // Folosiți calea nou generată
                };
 
-               string result = _eventCreate.CreateEventLogic(data);
+               string result = _location.AddLocation(data);
 
-               // Redirecționați către o pagină care arată toate evenimentele
-               return RedirectToAction("Index", "Evenimente");
+               // Redirecționați către o pagină care arată toate locatiile
+               return RedirectToAction("Index", "Locations");
           }
      }
-}
 }
